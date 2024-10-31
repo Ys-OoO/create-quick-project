68752ee0e89079dee8c8c8ec514f7deaf35a8cec
orinyang <orinyang@tencent.com>
2024-10-31 17:26:18 +0800

    shared

---
+++ templates/shared/eslint.config.js
@@ -0,0 +1,25 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginVue from 'eslint-plugin-vue';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+
+export default [
+  {
+    ignores: ['node_modules', 'dist', 'public'],
+  },
+  { files: ['**/*.{js,mjs,cjs,vue}'] },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        /** add global variables here */
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...pluginVue.configs['flat/essential'],
+  {
+    /** add custom rules here */
+  },
+  eslintPluginPrettierRecommended,
+];
---
+++ templates/shared/prettier.config.js
@@ -0,0 +1,12 @@
+// prettier.config.js
+export default {
+  trailingComma: 'all',
+  singleQuote: true,
+  semi: true,
+  printWidth: 80,
+  arrowParens: 'always',
+  proseWrap: 'always',
+  experimentalTernaries: false,
+  tabWidth: 2,
+  useTabs: false,
+};
