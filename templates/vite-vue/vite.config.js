e1c14f1f785c89b08a1b59d4260b10579154bf77
orinyang <orinyang@tencent.com>
2024-11-07 16:15:08 +0800

    template:vite-vue:add @vitejs/plugin-legacy

--- templates/vite-vue/vite.config.js
+++ templates/vite-vue/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import process from 'process';
+import viteLegacyPlugin from '@vitejs/plugin-legacy';
 
 const isProduction = process.env.NODE_ENV === 'production';
+// change to your path
 const PRODUCTION_URL = '/';
 
 export default defineConfig({
@@ -10,6 +12,11 @@ export default defineConfig({
   plugins: [
     vue({
       isProduction,
+    }),
+    viteLegacyPlugin({
+      targets: [
+        'ie >= 11, IOS >= 9,  Android>=4.4, last 2 versions, >0.2%, not dead',
+      ],
     }),
   ],
 });
