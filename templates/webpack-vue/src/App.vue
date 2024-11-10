c8671019122f9d3d973d5c9d7776a434f29a47ca
orinyang <orinyang@tencent.com>
2024-11-10 20:10:44 +0800

    init:webpack-vue

---
+++ templates/webpack-vue/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+</script>
+
+<template>
+  <HelloWorld msg="Webpack + Vue" />
+</template>
+
+<style scoped></style>
---
+++ templates/webpack-vue/webpack.config.js
@@ -0,0 +1,39 @@
+import path from "path";
+import { VueLoaderPlugin } from "vue-loader";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+export default {
+  mode: "development",
+  entry: "./src/main.js",
+  output: {
+    filename: "[name]-[chunkhash].js",
+    path: path.resolve("./dist"),
+    hashDigestLength: 5,
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.vue$/,
+        exclude: /(node_modules|bower_components)/,
+        use: "vue-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+};
