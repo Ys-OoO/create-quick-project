e3d04fb577b94640d23b7b6aea4141ae9820b64c
orinyang <orinyang@tencent.com>
2024-11-10 21:08:12 +0800

    add:vite-vue postcss options

--- templates/vite-vue/vite.config.js
+++ templates/vite-vue/vite.config.js
@@ -2,13 +2,22 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import process from 'process';
 import viteLegacyPlugin from '@vitejs/plugin-legacy';
-
+import autoprefixer from 'autoprefixer';
 const isProduction = process.env.NODE_ENV === 'production';
 // change to your path
 const PRODUCTION_URL = '/';
 
 export default defineConfig({
   base: isProduction ? PRODUCTION_URL : '/',
+  css: {
+    postcss: {
+      plugins: [
+        autoprefixer({
+          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+        }),
+      ],
+    },
+  },
   plugins: [
     vue({
       isProduction,
